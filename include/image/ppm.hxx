ec04b3039047e5a3052910070ab1fcb4baafa8ab
Houssam CUMZAIN <doriancoriace@gmail.com>
2024-01-21 22:54:28 +0100

    feat: add read functions for PPM files
    Both ascii and binary are supported when reading.

---
+++ include/image/ppm.hxx
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "image.hxx" // compositing::image
+
+#include <filesystem> // std::filesystem::path
+#include <optional>   // std::optional
+
+namespace compositing::ppm
+{
+    /*
+     * PPM images can be stored in ASCII or binary format.
+     */
+    enum class format
+    {
+        ascii,
+        binary
+    };
+
+    /*
+     * @brief Read a PPM image from a file.
+     * @param path The path to the file.
+     * @return The image if the file exists and is valid, std::nullopt otherwise.
+     */
+    [[nodiscard]] auto read(std::filesystem::path const& path) noexcept -> std::optional<image>;
+
+    /*
+     * @brief Write a PPM image to a file.
+     * @param image The image to write.
+     * @param path The path to the file.
+     * @param fmt The format to use. (ascii or binary)
+     * @return True if the image was written successfully, false otherwise.
+     */
+    [[nodiscard]] auto write(image const& image, std::filesystem::path const& path, format fmt = format::ascii) noexcept -> bool;
+} // namespace compositing::ppm
---
+++ source/image/ppm.cxx
@@ -0,0 +1,96 @@
+#include "image/ppm.hxx"
+
+#include <bit>     // std::bit_cast
+#include <cstddef> // std::size_t
+#include <fstream> // std::ifstream
+
+namespace compositing::ppm
+{
+    namespace
+    {
+        /*
+         * @brief Read a PPM image from a file in ASCII format.
+         * @param path The path to the file.
+         * @return The image if the file exists and is valid, std::nullopt otherwise.
+         */
+        [[nodiscard]] auto read_ascii(std::ifstream& file, std::uint32_t width, std::uint32_t height) noexcept
+            -> std::optional<image>
+        {
+            image img = {
+                .pixels = std::vector<pixel>(static_cast<std::size_t>(width) * height),
+                .width = width,
+                .height = height,
+            };
+
+            for (auto& pixel : img.pixels)
+            {
+                std::uint32_t red{0};
+                std::uint32_t green{0};
+                std::uint32_t blue{0};
+
+                file >> red;
+                file >> green;
+                file >> blue;
+
+                pixel = {
+                    .red = static_cast<std::uint8_t>(red),
+                    .green = static_cast<std::uint8_t>(green),
+                    .blue = static_cast<std::uint8_t>(blue),
+                };
+            }
+
+            return img;
+        }
+
+        /*
+         * @brief Read a PPM image from a file in binary format.
+         * @param path The path to the file.
+         * @return The image if the file exists and is valid, std::nullopt otherwise.
+         */
+        [[nodiscard]] auto read_binary(std::ifstream& file, std::uint32_t width, std::uint32_t height) noexcept
+            -> std::optional<image>
+        {
+            image img = {
+                .pixels = std::vector<pixel>(static_cast<std::size_t>(width) * height),
+                .width = width,
+                .height = height,
+            };
+            auto* const bytes = std::bit_cast<char*>(img.pixels.data());
+            auto const size = static_cast<std::streamsize>(img.pixels.size() * sizeof(pixel));
+
+            file.read(bytes, size);
+
+            return img;
+        }
+    } // namespace
+
+    /*
+     * @brief Read a PPM image from a file.
+     * @param path The path to the file.
+     * @return The image if the file exists and is valid, std::nullopt otherwise.
+     */
+    [[nodiscard]] auto read(std::filesystem::path const& path) noexcept -> std::optional<image>
+    {
+        if (!std::filesystem::exists(path)) [[unlikely]] { return std::nullopt; }
+
+        std::ifstream file{path};
+        if (!file.is_open()) [[unlikely]] { return std::nullopt; }
+
+        std::string magic_number;
+        std::getline(file, magic_number);
+
+        std::uint32_t width{0};
+        std::uint32_t height{0};
+        std::uint32_t max_color{0};
+        file >> width;
+        file >> height;
+        file >> max_color;
+
+        file.ignore();
+
+        if (magic_number == "P3") { return read_ascii(file, width, height); }
+        if (magic_number == "P6") { return read_binary(file, width, height); }
+
+        return std::nullopt;
+    }
+} // namespace compositing::ppm
