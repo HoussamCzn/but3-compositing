2cdaf5bceedb463eced6a4ef0df4cc5217726491
Houssam CUMZAIN <doriancoriace@gmail.com>
2024-01-22 00:02:15 +0100

    refactor: remove anonymous namespace

--- source/image/ppm.cxx
+++ source/image/ppm.cxx
@@ -6,104 +6,101 @@
 
 namespace compositing::ppm
 {
-    namespace
+    /*
+     * @brief Read a PPM image from a file in ASCII format.
+     * @param path The path to the file.
+     * @return The image from the file.
+     */
+    [[nodiscard]] auto read_ascii(std::ifstream& file, std::uint32_t width, std::uint32_t height) noexcept -> image
     {
-        /*
-         * @brief Read a PPM image from a file in ASCII format.
-         * @param path The path to the file.
-         * @return The image if the file exists and is valid, std::nullopt otherwise.
-         */
-        [[nodiscard]] auto read_ascii(std::ifstream& file, std::uint32_t width, std::uint32_t height) noexcept -> image
+        image img = {
+            .pixels = std::vector<pixel>(static_cast<std::size_t>(width) * height),
+            .width = width,
+            .height = height,
+        };
+
+        for (auto& pixel : img.pixels)
         {
-            image img = {
-                .pixels = std::vector<pixel>(static_cast<std::size_t>(width) * height),
-                .width = width,
-                .height = height,
+            std::uint32_t red{0};
+            std::uint32_t green{0};
+            std::uint32_t blue{0};
+
+            file >> red;
+            file >> green;
+            file >> blue;
+
+            pixel = {
+                .red = static_cast<std::uint8_t>(red),
+                .green = static_cast<std::uint8_t>(green),
+                .blue = static_cast<std::uint8_t>(blue),
             };
-
-            for (auto& pixel : img.pixels)
-            {
-                std::uint32_t red{0};
-                std::uint32_t green{0};
-                std::uint32_t blue{0};
-
-                file >> red;
-                file >> green;
-                file >> blue;
-
-                pixel = {
-                    .red = static_cast<std::uint8_t>(red),
-                    .green = static_cast<std::uint8_t>(green),
-                    .blue = static_cast<std::uint8_t>(blue),
-                };
-            }
-
-            return img;
         }
 
-        /*
-         * @brief Read a PPM image from a file in binary format.
-         * @param path The path to the file.
-         * @return The image if the file exists and is valid, std::nullopt otherwise.
-         */
-        [[nodiscard]] auto read_binary(std::ifstream& file, std::uint32_t width, std::uint32_t height) noexcept -> image
-        {
-            image img = {
-                .pixels = std::vector<pixel>(static_cast<std::size_t>(width) * height),
-                .width = width,
-                .height = height,
-            };
-            auto* const bytes = std::bit_cast<char*>(img.pixels.data());
-            auto const size = static_cast<std::streamsize>(img.pixels.size() * sizeof(pixel));
+        return img;
+    }
 
-            file.read(bytes, size);
+    /*
+     * @brief Read a PPM image from a file in binary format.
+     * @param path The path to the file.
+     * @return The image from the binary file.
+     */
+    [[nodiscard]] auto read_binary(std::ifstream& file, std::uint32_t width, std::uint32_t height) noexcept -> image
+    {
+        image img = {
+            .pixels = std::vector<pixel>(static_cast<std::size_t>(width) * height),
+            .width = width,
+            .height = height,
+        };
+        auto* const bytes = std::bit_cast<char*>(img.pixels.data());
+        auto const size = static_cast<std::streamsize>(img.pixels.size() * sizeof(pixel));
 
-            return img;
-        }
+        file.read(bytes, size);
 
-        /*
-         * @brief Write a PPM image to a file in ASCII format.
-         * @param image The image to write.
-         * @param path The path to the file.
-         * @return True if the image was written successfully, false otherwise.
-         */
-        [[nodiscard]] auto write_ascii(image const& image, std::filesystem::path const& path) noexcept -> bool
-        {
-            std::ofstream file{path};
-            if (!file.is_open()) [[unlikely]] { return false; }
+        return img;
+    }
 
-            file << "P3\n";
-            file << image.width << " " << image.height << "\n";
-            file << "255\n";
+    /*
+     * @brief Write a PPM image to a file in ASCII format.
+     * @param image The image to write.
+     * @param path The path to the file.
+     * @return True if the image was written successfully, false otherwise.
+     */
+    [[nodiscard]] auto write_ascii(image const& image, std::filesystem::path const& path) noexcept -> bool
+    {
+        std::ofstream file{path};
+        if (!file.is_open()) [[unlikely]] { return false; }
 
-            for (auto const& pixel : image.pixels) { file << pixel << "\n"; }
+        file << "P3\n";
+        file << image.width << " " << image.height << "\n";
+        file << "255\n";
 
-            return true;
-        }
+        for (auto const& pixel : image.pixels) { file << pixel << "\n"; }
 
-        /*
-         * @brief Write a PPM image to a file in binary format.
-         * @param image The image to write.
-         * @param path The path to the file.
-         * @return True if the image was written successfully, false otherwise.
-         */
-        [[nodiscard]] auto write_binary(image const& image, std::filesystem::path const& path) noexcept -> bool
-        {
-            std::ofstream file{path};
-            if (!file.is_open()) [[unlikely]] { return false; }
+        return true;
+    }
 
-            file << "P6\n";
-            file << image.width << " " << image.height << "\n";
-            file << "255\n";
+    /*
+     * @brief Write a PPM image to a file in binary format.
+     * @param image The image to write.
+     * @param path The path to the file.
+     * @return True if the image was written successfully, false otherwise.
+     */
+    [[nodiscard]] auto write_binary(image const& image, std::filesystem::path const& path) noexcept -> bool
+    {
+        std::ofstream file{path};
+        if (!file.is_open()) [[unlikely]] { return false; }
 
-            auto const* const bytes = std::bit_cast<char const*>(image.pixels.data());
-            auto const size = static_cast<std::streamsize>(image.pixels.size() * sizeof(pixel));
+        file << "P6\n";
+        file << image.width << " " << image.height << "\n";
+        file << "255\n";
 
-            file.write(bytes, size);
+        auto const* const bytes = std::bit_cast<char const*>(image.pixels.data());
+        auto const size = static_cast<std::streamsize>(image.pixels.size() * sizeof(pixel));
 
-            return true;
-        }
-    } // namespace
+        file.write(bytes, size);
+
+        return true;
+    }
 
     /*
      * @brief Read a PPM image from a file.
