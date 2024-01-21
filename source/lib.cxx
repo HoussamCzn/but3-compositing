0f5a59c6d70c642b2a3c3cc39f393e6a6e6aa852
Houssam CUMZAIN <doriancoriace@gmail.com>
2024-01-21 22:49:07 +0100

    chore: remove pre-generated files

--- source/lib.cxx
+++
@@ -1,5 +0,0 @@
-#include "lib.hxx"
-
-#include <format>
-
-library::library() : name{std::format("{}", "compositing")} {}
--- test/source/compositing_test.cxx
+++
@@ -1,9 +0,0 @@
-#include "lib.hxx"
-
-#include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("Name is compositing", "[library]")
-{
-    auto const lib = library{};
-    REQUIRE(lib.name == "compositing");
-}
