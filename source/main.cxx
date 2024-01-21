932c0d157d3d979d60e2579f5aa9698dc029d094
Houssam CUMZAIN <doriancoriace@gmail.com>
2024-01-21 22:49:50 +0100

    fix: temporarily change main function
    Removed old stuff which included the pre-generated functions that are now removed.

--- source/main.cxx
+++ source/main.cxx
@@ -1,12 +1,8 @@
-#include "lib.hxx"
-
 #include <iostream>
 
 auto main() -> int
 {
-    auto const lib = library{};
-    auto const message = "Hello from " + lib.name + "!";
-    std::cout << message << '\n';
+    std::cout << "Hello, World!\n";
 
     return 0;
 }
